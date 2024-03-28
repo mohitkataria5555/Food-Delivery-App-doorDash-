4d1758de98e7482c8c44d3a944fdbf65aa8e9eca
mohitkataria5555 <mohitkataria5555@gmail.com>
2024-03-28 20:11:22 +0530

    user repo

---
+++ src/main/java/com/doorDash/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.doorDash.repository;
+
+import com.doorDash.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    public User findByEmail(String username);
+}
